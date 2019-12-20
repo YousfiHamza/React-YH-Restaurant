869f1ca83dac8db92ca40775190e3982a0584e27
Hamza <hamzayousfi2@gmail.com>
2019-12-20 12:55:11 +0200

    setting up the workshop

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello From NavBar</h3>
+      </div>
+    );
+  }
+}
---
+++ src/data.js
@@ -0,0 +1,98 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Boulfaf",
+    img: "img/boulfaf.png",
+    price: 11,
+    chef: "Chef Hamza",
+    info:
+      "Ce plat d’origine arabe à base de viande est le plus souvent préparé pour les grandes fêtes la fête du mouton (Aïd el kebir). On le déguste généralement à l’agneau ou au mouton enroulé dans des petits morceaux de crépine de mouton fraîche et très fine. Accompagné de salades et de légumes variés.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Couscous",
+    img: "img/couscous.png",
+    price: 21,
+    chef: "Chef Fatima",
+    info:
+      "Le couscous est le principal ambassadeur de la cuisine marocaine. C’est en tout cas à lui que l’on pense en premier quand on évoque la gastronomie du Maroc. Dégusté généralement le vendredi midi, il est d’origine berbère et se prépare traditionnellement avec de la viande de mouton, mais aussi de poulet (on oublie le couscous au merguez et aux boulettes qui est une invention française).",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Harira",
+    img: "img/harira.png",
+    price: 9,
+    chef: "Chef Hind",
+    info:
+      "Cette variante de la chorba est une recette typique du Maroc, que l’on déguste principalement pour le ftour (la fin du jeûne durant le ramadan). A base de légumes, d’épices, d’aromates, et parfois de viande, elle est à la fois réconfortante et nutritive, et donc idéale pour se remplir l’estomac tout en se régalant.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Pastilla",
+    img: "img/pastilla.png",
+    price: 29,
+    chef: "Chef Ahmed",
+    info:
+      "Parmi les meilleurs plats marocains, on retrouve également la pastilla, qui vient traditionnellement de la ville de Fès. Préparée initialement avec de la viande de pigeon, on la déguste aujourd’hui plutôt avec du poulet effiloché, agrémentée de cannelle, de fruits secs, le tout saupoudré de sucre juste avant de servir. Il existe également des recettes de pastilla sucrées ou la même base de feuilles de brick est cette fois-ci fourrée à la crème de lait et à la maïzena.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Rfissa",
+    img: "img/rfissa.png",
+    price: 31,
+    chef: "Chef Abderrahmane",
+    info:
+      "Ce plat originaire de Casablanca est parfaitement représentatif de la gastronomie du Maroc. Très riche en saveurs et en épices, il est préparé avec des feuilles de msemen, agrémentées d’un ragoût à la viande de poulet et de lentilles, parfumées aux épices. On le donne généralement à la mère qui vient de donner naissance, pour l’aider à reprendre des forces.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Shlada",
+    img: "img/salade.png",
+    price: 9,
+    chef: "Chef Bello",
+    info:
+      "Si la cuisine marocaine est connue pour être riche et pas toujours idéale pour un régime, elle est également équilibrée et saine puisque composée de nombreuses salades (cuites ou crues) qui accompagnent toujours un plat. Riches en légumes, mais aussi en épices, elles peuvent être déclinées à l’infini, même si certaines recettes sont plus populaires que d’autres, comme la salade de tomates et pois chiche, les carottes au cumin, la salade de courgette et de noisette, ou encore la zaalouk d’aubergine.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Seffa",
+    img: "img/seffa.png",
+    price: 22,
+    chef: "Chef Ramsey",
+    info:
+      "La seffa est un plat que l’on partage, assez simple à préparer mais qui demande beaucoup de patience, puisqu’il va cuire pendant de longues heures. S’il n’est pas aussi connu que le couscous, il est tout aussi savoureux mais aussi équilibré puisque riche en fruits secs et en épices. On le prépare avec des vermicelles (baptisés cheveux d’ange au Maroc), qui vont être servis avec de la viande de poulet, et agrémenté pour les grandes occasions de sucre glaces et d’amandes concassées. Un délice !",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Tajine",
+    img: "img/tajine.png",
+    price: 22,
+    chef: "Chef Ala Rasso",
+    info:
+      "Le tajine est lui aussi emblématique de la cuisine marocaine. Ce plat en terre cuite (assez proche de notre cocotte) permet de réaliser près de 300 recettes différentes, généralement salées, mais parfois sucrées, dans lesquelles on retrouve de la viande et des légumes, agrémentés de fruits secs, d’épices et d’aromates.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
