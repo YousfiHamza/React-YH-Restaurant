a5d4a6153fd43f25624c1bdfd10be8521c9d0ff9
Hamza <hamzayousfi2@gmail.com>
2019-12-27 21:56:30 +0200

    Setting a default page for wrong urls (#404)

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import Title from "./Title";
+import camels from "../camels.png";
 
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>Page Not Found !</h3>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title">
+            <Title title=" Erreur #404 " />
+            <img
+              src={camels}
+              alt="Camels Traveling"
+              style={{ width: "70vw" }}
+            />
+            <Title title=" Page Introuvable !" />
+          </div>
+        </div>
       </div>
     );
   }
